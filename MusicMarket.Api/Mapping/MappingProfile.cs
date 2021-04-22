2afafea57f1aabffddbd68503338041238e4803d
Ahmet Erkan <ahmete48@hotmail.com>
2021-04-22 11:37:00 +0300

    Initial commit

---
+++ MusicMarket.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MusicMarket.Api.DTO;
+using MusicMarket.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //domain to resource
+            CreateMap<Music, MusicDTO>();
+            CreateMap<Artist, ArtistDTO>();
+            CreateMap<SaveArtistDTO, Artist>();
+
+            //resource to domain
+            CreateMap<MusicDTO, Music>();
+            CreateMap<ArtistDTO, Artist>();
+            CreateMap<SaveMusicDTO, Music>();
+
+        }
+    }
+}
---
+++ MusicMarket.Api/Validators/SaveArtistResourceValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MusicMarket.Api.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Api.Validators
+{
+    public class SaveArtistResourceValidator : AbstractValidator<SaveArtistDTO>
+    {
+        public SaveArtistResourceValidator()
+        {
+            RuleFor(a => a.Name).NotEmpty().MaximumLength(50);
+        }
+    }
+}
